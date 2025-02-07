c8b9e102cba4ae43960760df3e6f1aa4c03194f3
hrpalmaen <hrpalmaen@gmail.com>
2025-02-07 18:00:20 +0000

    Added Docker build and push

--- app-cdk/app.py
+++ app-cdk/app.py
@@ -5,6 +5,7 @@ import aws_cdk as cdk
 
 from app_cdk.app_cdk_stack import AppCdkStack
 from app_cdk.pipeline_cdk_stack import PipelineCdkStack
+from app_cdk.ecr_cdk_stack import EcrCdkStack
 
 app = cdk.App()
 AppCdkStack(app, "AppCdkStack",
@@ -25,9 +26,15 @@ AppCdkStack(app, "AppCdkStack",
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
 
+ecr_stack = EcrCdkStack(
+    app,
+    'ecr-stack'
+)
+
 pipeline_stack = PipelineCdkStack(
     app,
     'pipeline-stack',
+    ecr_repository = ecr_stack.ecr_data,
 )
 
 app.synth()
